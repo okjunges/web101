a0f5599d3cce04dc35ba201e50947448a8d0b6a2
okjunges <rladhkstn02@gamil.com>
2025-01-16 03:35:17 +0900

    Project Initialization

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication // 스프링 부트의 자동 설정
 public class Application {
     public static void main(String[] args) {
+        // project initialization
         SpringApplication.run(Application.class, args);
     }
 }
