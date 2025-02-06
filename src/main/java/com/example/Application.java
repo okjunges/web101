ea555b2bcf1c831fc304aa2ac471900eade6ab06
okjunges <rladhkstn02@gamil.com>
2025-02-06 19:00:28 +0900

    Week 5 Mission

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication // 스프링 부트의 자동 설정
+@EnableJpaAuditing
 public class Application {
     public static void main(String[] args) {
         // project initialization
